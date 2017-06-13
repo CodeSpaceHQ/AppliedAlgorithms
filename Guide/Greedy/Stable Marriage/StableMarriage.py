67e7d6c0c2a8a13626fcbcc3d25bde078fbeb32b
dlowrey <dane.r.lowrey@gmail.com>
2017-06-12 20:08:50 -0500

    Commented & formatted printing of results

--- Guide/Greedy/Stable Marriage/StableMarriage.py
+++ Guide/Greedy/Stable Marriage/StableMarriage.py
@@ -7,17 +7,22 @@ def stable_marriage(men, women):
     while single_men:
         man = single_men[0]
 
+        # Get man's highest ranked woman
         preferred_wife = men[man][0]
         wife_preferences = women[preferred_wife]
 
+        # Let man propose to her
         if preferred_wife not in wives:
+            # if she is free, she will accept
             wives[preferred_wife] = man
             del men[man][0]
             single_men.remove(man)
         else:
+            # Otherwise we have to check if she prefers this man
+            # over her current man.
             fiance = wives[preferred_wife]
 
-            # Position of competing men in the woman's preferences
+            # Position of both competing men in the woman's preferences
             place_of_fiance = wife_preferences.index(fiance)
             place_of_man = wife_preferences.index(man)
 
@@ -29,28 +34,34 @@ def stable_marriage(men, women):
                 single_men.append(fiance)
             else:
                 del men[man][0]  # Remove woman from man's preferences
+
+    # Return all engaged pairs
     return wives
 
 
 def main():
     # Set of men comprised of their preferences
     men = {
-        0:[2, 1, 0],
-        1:[2, 1, 0],
-        2:[2, 1, 0]
+        1: [2, 3, 4, 1],
+        2: [2, 1, 3, 4],
+        3: [1, 3, 4, 2],
+        4: [1, 2, 3, 4]
     }
 
     # Set of women comprised of their preferences
     women = {
-        0: [1, 0, 2],
-        1: [1, 0, 2],
-        2: [1, 0, 2]
+        1: [2, 4, 1, 3],
+        2: [3, 1, 2, 4],
+        3: [1, 2, 4, 3],
+        4: [1, 2, 4, 3]
     }
 
+    # Run stable marriage algorithm
     pairs = stable_marriage(men, women)
-    print('(W, M) = Engagement')
-    for engagement in pairs.items():
-        print(engagement)
+
+    print('(M, W) = Engagement')
+    for key in pairs:
+        print('(' + str(pairs[key]) + ',' + str(key) + ')')
 
 
 if __name__ == '__main__':
