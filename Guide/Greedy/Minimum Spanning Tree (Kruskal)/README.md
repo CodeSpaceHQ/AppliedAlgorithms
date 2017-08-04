5bfcc1d135beda4aea95a0ebf303a2f8faf9aec8
dlowrey <dane.r.lowrey@gmail.com>
2017-08-03 23:29:03 -0500

    Commented implementation and updated README (except images)

--- Guide/Greedy/Minimum Spanning Tree (Kruskal)/README.md
+++ Guide/Greedy/Minimum Spanning Tree (Kruskal)/README.md
@@ -5,7 +5,7 @@ This problem is discussed in [Dr. Gelfond Applied Algorithms](http://redwood.cs.
 
 Category: Greedy
 
-Difficulty: N/A
+Difficulty to Understand: Hard
 
 ## Problem
 Given a graph G = (V, E) with positive cost c<sub>e</sub> associated with every edge e and:
@@ -16,10 +16,41 @@ Given a graph G = (V, E) with positive cost c<sub>e</sub> associated with every 
 Find a minimum spanning tree of G.
 
 ### Overview
-The total number of spanning trees in a graph G is O(n<sup>n</sup>). An exhaustive search is impossible.
+There are many algorithms to find the minimum spanning tree of a graph G. The one we will be using is [Kruskal's Algorithm](https://en.wikipedia.org/wiki/Kruskal%27s_algorithm#Pseudocode).
+We will implement the most-optimized version of the algorithm in Dr. Gelfond's lecture slides using disjoint-set data structure with Union() and Find() functions.
+
+**[Disjoint-set data structure](https://en.wikipedia.org/wiki/Disjoint-set_data_structure#MakeSet)**:  is a data structure that keeps track of a set of elements partitioned into a number of disjoint (non-overlapping) subsets.
+
+Here is a disjoint-set of 8 elements taken from the above Wikipedia page:
+
+![Initial Disjoint Set](https://github.com/CodeSpaceHQ/AppliedAlgorithms/blob/interval-partitioning/Guide/Greedy/Interval%20Partitioning/Assets/step1.PNG "8 individual disjoint sets")
+
+As we add edges (comprised of vertecies) to our minimum spanning tree, we will also Union() the two vertecies in the edge together. After numerous iterations of this, the
+above initial disjoint set would now look something like this:
+
+![Disjoint Set Grouped](https://github.com/CodeSpaceHQ/AppliedAlgorithms/blob/interval-partitioning/Guide/Greedy/Interval%20Partitioning/Assets/step1.PNG "8 overlapping / partially grouped disjoint sets")
+
+The reason for doing this will be discussed further in the Analysis section of this guide. For a more in-depth discussion of disjoint-set data types and
+related topics, please visit the [Disjoint-set](https://en.wikipedia.org/wiki/Disjoint-set_data_structure#MakeSet) Wikipedia page.
+
 
 ### Input Format
+Our input format will a graph G = [V, E] where V is a set of vertecies and E is a set of edges with their corresponding weights such that:
+* Each vertex _v_ in V will have a pointer to a parent vertex as well as a rank and an id (i.e. an id of _a_, _b_, _c_...).
+* Each edge will be represented in the form _(v, w, v)_ where _v_ is a vertex and w is the edge's weight.
 
+An example input graph to our algorithm might look like this:
+
+```Python
+    vertex_a = Vertex('a')  # 'a' is the id of vertex_a, and initially vertex_a is it's own parent
+    vertex_b = Vertex('b')
+
+    # our input graph G
+    G = [
+        [a, b],  # set of vertecies
+        [(vertex_a, 4, vertex_b)]  # set of edges, with one edge from vertex_a to vertex_b with a weight of 4
+    ]
+```
 
 ### Constraints
 
--- Guide/Greedy/Minimum Spanning Tree (Kruskal)/kruskal_min_spanning_tree.py
+++ Guide/Greedy/Minimum Spanning Tree (Kruskal)/kruskal_min_spanning_tree.py
@@ -1,23 +1,27 @@
-class Node:
+
+# A simple class for a vertex in our graph
+# we represent each vertex as a node with an id, a rank, and a pointer
+# to a parent node (like a linked list) for our disjoint-set data structure
+# see https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+class Vertex:
     def __init__(self, id, rank=0):
-        self.id = id
-        self.parent = self
-        self.rank = rank
-
-    def __str__(self):
-        return self.id
+        self.id = id  # identification of the vertex
+        self.rank = rank  # rank of the vertex (for the union function)
+        self.parent = self  # parent or "root" vertex of this vertex
 
 
-def make_set(vertex):
-    return Node(vertex)
-
-
+# Part of Union-Find disjoint-set data structure
+# Find the 'topmost' parent of the disjoint-set
 def find(node):
     if node.parent != node:
+        # reassign the parent so we can flatten the structure
         node.parent = find(node.parent)
     return node.parent
 
 
+# Part of Union-Find disjoint-set data structure
+# Union the two disjoint sets, always union the shorter of the two
+# to the longer if possible.
 def union(x, y):
     x_root = find(x)
     y_root = find(y)
@@ -29,6 +33,7 @@ def union(x, y):
     else:
         y_root.parent = x_root
         x_root.rank += 1
+
 
 def partition(s, begin, end):
     # Lomuto partition scheme worst case O(n^2)
@@ -45,7 +50,6 @@ def partition(s, begin, end):
 
 
 def quick_sort(s, begin, end):
-    # quicksort
     if begin < end:
         pivot = partition(s, begin, end)
         # sort sub array on the left
@@ -56,32 +60,37 @@ def quick_sort(s, begin, end):
 
 
 def minimum_spanning_tree(graph):
-    tree = set()  # set of edges
-
-    for v in graph[0]:
-        make_set(v)
-
-    # sort edges by weight
-    graph[1] = quick_sort(graph[1], 0, len(graph[1])-1)
+    solution = set()  # set of edges for MST
+    quick_sort(graph[1], 0, len(graph[1])-1)  # sort by weight
     for e in graph[1]:
+        # if the two vertex are not apart of the same disjoint-set
+        # and therefore won't make a cycle
         if find(e[0]) != find(e[2]):
+            # make them apart of the same disjoint-set
             union(e[0], e[2])
-            tree.add(e)
-    return tree
+            # add the edge to the solution
+            solution.add(e)
+    return solution
 
 
 def main():
-    a = Node('a')
-    b = Node('b')
-    c = Node('c')
-    d = Node('d')
-    e = Node('e')
+    # Create each vertex in our graph
+    a = Vertex('a')
+    b = Vertex('b')
+    c = Vertex('c')
+    d = Vertex('d')
+    e = Vertex('e')
+
+    # Represent our graph G as a pair of vertecies and edges with weights.
     G = [
-        [a, b, c, d, e],
-        [(a, 10, b), (b, 1, c), (c, 2, d), (d, 3, e), (d, 1, a)]
+        [a, b, c, d, e],  # vertecies
+        [(a, 10, b), (b, 1, c), (c, 2, d), (d, 3, e), (d, 1, a)]  # edges
     ]
+
+    # find the min spanning tree
     mst = minimum_spanning_tree(G)
 
+    # pretty print
     for e in mst:
         print('{}--{}--{}'.format(e[0].id, e[1], e[2].id))
 
