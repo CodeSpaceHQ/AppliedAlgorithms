60fadb1054720e7af1650664d3995ce11e493b6f
dlowrey <dane.r.lowrey@gmail.com>
2017-08-13 16:06:53 -0500

    Updated comments

--- Guide/Greedy/Minimizing Maximum Lateness Scheduling/min_max_scheduling.py
+++ Guide/Greedy/Minimizing Maximum Lateness Scheduling/min_max_scheduling.py
@@ -1,57 +1,69 @@
-
 def partition(s, begin, end):
-    # Lomuto partition scheme worst case O(n^2)
-    # this version of partition may not be the most efficient in
-    # terms of edge cases - but will do for our purpose of demonstration.
-    pivot = end
-    pivot_holder_index = begin
+    """
+    Partially sort an array using a pivot value. Everything to the 
+    left of the pivot will be less than the pivot. Everything to the right
+    will be greater than the pivot.
+    :param s: the array to sort
+    :param begin: the beginning of the section of the array to sort
+    :param end: the end of the section of the array to sort
+    :return: the array with sorted section begin-end
+    """
+    pivot = end  # set the pivot to the end of the array
+    pivot_holder_index = begin  # place holder for where the pivot will end up
     for i in range(begin, end):
-        if s[i][1] < s[pivot][1]:
+        if s[i][1] < s[pivot][1]:  # if s[i] < s[pivot]
+            # switch s[i] and pivot place holder
             s[i], s[pivot_holder_index] = s[pivot_holder_index], s[i]
-            pivot_holder_index += 1
+            pivot_holder_index += 1  # increase pivot place holder
+
+    # finally, put pivot in its placeholder
     s[pivot_holder_index], s[end] = s[end], s[pivot_holder_index]
     return pivot_holder_index
 
 
 def quick_sort(s, begin, end):
-    # quicksort
+    """
+    Quick sort 
+    :param s: the array to sort
+    :param begin: the beginning of the array
+    :param end: the ending of the array
+    :return: the sorted array
+    """
+    # Quick sort
     if begin < end:
-        pivot = partition(s, begin, end)
-        # sort sub array on the left
-        quick_sort(s, begin, pivot - 1)
-        # sort sub array on the right
-        quick_sort(s, pivot + 1, end)
+        pivot = partition(s, begin, end)  # find a pivot place
+        quick_sort(s, begin, pivot - 1)  # recursive sort sub array on the left
+        quick_sort(s, pivot + 1, end)  # recursive sort sub array on the right
     return s
 
 
-def min_max_lateness(set_of_requests):
+def minimize_lateness(set_of_requests):
+    """
+    Make a schedule for a set of requests so that the lateness (the time the request goes
+    over it's deadline) is minimized.
+    :param set_of_requests: the set of requests to build a schedule for
+    :return: the schedule for all of the requests
+    """
+
+    # Sort request by deadline in ascending order
     sorted_requests = quick_sort(set_of_requests, 0, len(set_of_requests)-1)
-    last_finish_time = 0
-    schedule = []
+    last_finish_time = 0  # initial finish time
+    schedule = []  # initial schedule for requests
     for i in sorted_requests:
-        # start time of this request is the last finish time.
-        start_time = last_finish_time
-        # finish time of this request is the start time plus the request's
-        # duration
-        finish_time = start_time + i[0]
-        # last finish time is now the previous finish time plus the time it
-        # took to complete this task
-        last_finish_time += i[0]
-        # for fun we will calculate the lateness of the request,
-        # the time it ran over its deadline, finish time - deadline
-        lateness = finish_time - i[1]
-        # append [original request, (start time, finish time)] so we
-        # can keep track and print later
+        start_time = last_finish_time  # start of requests is last finish time
+        finish_time = start_time + i[0]  # this requests finish time
+        last_finish_time += i[0]  # update latest finish time
+        lateness = finish_time - i[1]  # calculate this requests lateness
         schedule.append([i,(start_time, finish_time), lateness])
     return schedule
 
 
 def main():
-    # requests in form of (time, deadline)
+    # Request: (time, deadline)
     requests = [(3, 10), (4, 5), (1, 6), (6, 2), (1, 11)]
-    scheduled = min_max_lateness(requests)
+    scheduled = minimize_lateness(requests)
 
-    # pretty print it all
+    # Pretty print
     print('Request\tStart\tFinish\tLateness')
     for i in scheduled:
         print('{}\t{}\t\t{}\t\t{}'.format(i[0], i[1][0], i[1][1], i[2]))
