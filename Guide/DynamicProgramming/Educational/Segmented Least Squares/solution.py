030275ce86e18000330a54bc0eb87701de888e30
dlowrey <dane.r.lowrey@gmail.com>
2017-10-08 14:51:06 -0500

    Updated docstrings and comments

--- Guide/DynamicProgramming/Educational/Segmented Least Squares/solution.py
+++ Guide/DynamicProgramming/Educational/Segmented Least Squares/solution.py
@@ -1,5 +1,6 @@
 from math import inf
 
+
 def partition(s, begin, end):
     """
     Partially sort an array using a pivot value. Everything to the 
@@ -40,10 +41,16 @@ def quick_sort(s, begin, end):
 
 
 def compute_err(points):
+    """
+    Compute the sum of least squared errors line. The equation for this line can
+    be found online.
+    :param points: a list of points (x,y) that are sorted by x values
+    :return: an error value for the line segment fitting the points
+    """
     n = len(points)  # number of points in segment
-    x_cords = [points[p][0] for p in range(0, n)] # all x values for segment
-    y_cords = [points[p][1] for p in range(0, n)] # all y values for segment
-    xy = map(lambda x, y: x * y, x_cords, y_cords) # xi*yi values for segment
+    x_cords = [points[p][0] for p in range(0, n)]  # all x values for segment
+    y_cords = [points[p][1] for p in range(0, n)]  # all y values for segment
+    xy = map(lambda x, y: x * y, x_cords, y_cords)  # xi*yi values for segment
     sum_x = sum(x_cords)  # sum of all x values for segment
     sum_y = sum(y_cords)  # sum of all y values for segment
     # calculate slope of least squared error line
@@ -51,9 +58,9 @@ def compute_err(points):
     a_denominator = n * (sum([x ** 2 for x in x_cords])) - (sum_x) ** 2
     a = a_numerator / a_denominator
     # calculate y-intercept of least squared error line
-    b = (sum_y - (a*sum_x)) / n
+    b = (sum_y - (a * sum_x)) / n
     # calculate SEE using the least squared error line
-    e = sum(map(lambda x, y: (y-a*x-b)**2, x_cords, y_cords))
+    e = sum(map(lambda x, y: (y - a * x - b) ** 2, x_cords, y_cords))
     return e
 
 
@@ -70,12 +77,12 @@ def segment_least_squares(points, cost):
 
     m = [0] * size
     opt_seg = [0] * size
-    for j in range(1, size): # for each point
+    for j in range(1, size):  # for each point
         min_err = inf
         opt_segment_end = None
-        for i in range(0, j): # for each point < the j_th point
+        for i in range(0, j):  # for each point < the j_th point
             # get the error of the total segment
-            err_of_segment = squared_errors[(i, j)] + cost + m[i-1]
+            err_of_segment = squared_errors[(i, j)] + cost + m[i - 1]
             if err_of_segment < min_err:  # get the segment with min error
                 min_err = err_of_segment
                 opt_segment_end = i  # save the point where segment starts
@@ -92,29 +99,28 @@ def find_solution(points, s):
     :param s: the starting point of each segment at s[i]
     :return: a list of points that when connected make up line segments
     """
-    if len(points) == 1:
+    if len(points) == 1:  # base case
         return points
     else:
         return find_solution(points[:s[-1] + 1], s[:s[-1] + 1]) + [points[-1]]
 
 
 def main():
-    coord_set = [
+    coord_set = [  # make it out of order by x value
         (5, 3),
         (6, 3),
         (7, 4),
         (8, 5),
         (9, 6),
         (10, 7),
-        (0,0),
-        (1,1),
-        (2,2),
-        (3,3),
-        (4,3)
-
+        (0, 0),
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 3)
     ]
-    s, m = segment_least_squares(coord_set, 1)
-    print(find_solution(coord_set, s))
+    s, m = segment_least_squares(coord_set, 1)  # find the segments, cost
+    print(find_solution(coord_set, s))  # print out points for ends of segment
 
 
 if __name__ == '__main__':
